362ef34921e2ac8282673c8244c06e39ac3f8684
Robin <24991308+Rolami@users.noreply.github.com>
2023-10-03 00:42:17 +0200

    Added second secret

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import React, { Component } from 'react';
 import './App.css';
 import cvData from './cvData.json';
 import PersonalInfo from './Components/CV/PersonalInfo';
 import SecretWordDetector from './Components/SecretWordDetector';
+import { Audio } from 'react-audio-player';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Robin react cv</h1>
+export default function App() {
+  // Function to play the sound
+  const playSound = () => {
+    const audioElement = document.querySelector('#my-sound-file');
+    audioElement.play();
+  };
 
-        <SecretWordDetector />
-  
-        <h1 className="section-title">Contact Info</h1>
-        <section>
-          <article>
-            <PersonalInfo data={cvData.personalInformation} />
+  // Event listener to play the sound on right-click
+  const handleContextMenu = (e) => {
+    e.preventDefault(); // Prevent the default context menu
+    console.log('Right-click detected!');
+    playSound(); // Play the sound
+  };
+
+  return (
+    <div className="App" onContextMenu={handleContextMenu}>
+      <h1>Robin react cv</h1>
+      <SecretWordDetector />
+
+      <h1 className="section-title">Contact Info</h1>
+      <section>
+        <article>
+          <PersonalInfo data={cvData.personalInformation} />
+        </article>
+      </section>
+
+      <h1 className="section-title">Education</h1>
+      <section>
+        {/* Map over education data and create articles */}
+        {cvData.education.map((educationItem, index) => (
+          <article key={`education-${index}`}>
+            <h3>{educationItem.school}</h3>
+            <p>{educationItem.degree}</p>
+            <p>{educationItem.year}</p>
           </article>
+        ))}
+      </section>
 
-        </section>
+      <h1 className="section-title">Work experience</h1>
+      <section>
+        {/* Map over experience data and create articles */}
+        {cvData.experience.map((experienceItem, index) => (
+          <article key={`experience-${index}`}>
+            <h3>{experienceItem.company}</h3>
+            <p>{experienceItem.position}</p>
+            <p>{experienceItem.year}</p>
+          </article>
+        ))}
+      </section>
 
-        <h1 className="section-title">Education</h1>
-        <section>
-          {/* Map over education data and create articles */}
-          {cvData.education.map((educationItem, index) => (
-            <article key={`education-${index}`}>
-              <h3>{educationItem.school}</h3>
-              <p>{educationItem.degree}</p>
-              <p>{educationItem.year}</p>
-            </article>
-          ))}
-        </section>
-
-        <h1 className="section-title">Work experience</h1>
-        <section>
-          {/* Map over experience data and create articles */}
-          {cvData.experience.map((experienceItem, index) => (
-            <article key={`experience-${index}`}>
-              <h3>{experienceItem.company}</h3>
-              <p>{experienceItem.position}</p>
-              <p>{experienceItem.year}</p>
-            </article>
-          ))}
-        </section>
-      </div>
-    );
-  }
+      <audio id="my-sound-file" src="./squeaky-toy.mp3"></audio>
+    </div>
+  );
 }
-
-export default App;
--- src/Components/SecretWordDetector.js
+++ src/Components/SecretWordDetector.js
@@ -31,6 +31,7 @@ function App() {
     }
   }, [keysPressed]);
 
+  
   return (
     <div />
   );

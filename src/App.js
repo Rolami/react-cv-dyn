c29f8844f9b60cf72c9c89571773089465dd01d3
Rolami <Larssonrobin88@gmail.com>
2023-10-04 00:10:42 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
+import cvData from "./cvData.json";
+import PersonalInfo from "./Components/CV/PersonalInfo";
+import SecretWordDetector from "./Components/CV/SecretWordDetector";
 import './App.css';
-import cvData from './cvData.json';
-import PersonalInfo from './Components/CV/PersonalInfo';
-import SecretWordDetector from './Components/SecretWordDetector';
-import { Audio } from 'react-audio-player';
 
 export default function App() {
-  // Function to play the sound
-  const playSound = () => {
-    const audioElement = document.querySelector('#my-sound-file');
-    audioElement.play();
-  };
-
-  // Event listener to play the sound on right-click
-  const handleContextMenu = (e) => {
-    e.preventDefault(); // Prevent the default context menu
-    console.log('Right-click detected!');
-    playSound(); // Play the sound
+  handleClick() {
+    alert('Why would you do that?', this)
+    return; // Add this line to fix the error
   };
 
   return (
-    <div className="App" onContextMenu={handleContextMenu}>
+    <div className="App">
       <h1>Robin react cv</h1>
       <SecretWordDetector />
+
+      <button onClick={() => this.handleClick()}>
+        Don't click me
+      </button>
+
 
       <h1 className="section-title">Contact Info</h1>
       <section>
@@ -54,7 +50,6 @@ export default function App() {
         ))}
       </section>
 
-      <audio id="my-sound-file" src="./squeaky-toy.mp3"></audio>
     </div>
   );
 }
